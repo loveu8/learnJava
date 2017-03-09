27d42814b4044a57d3cdaf57cbb49aa666f722d2
LornGhu <lornghu@eztravel.com.tw>
2017-03-09 17:42:36 +0800

    作業5 1~3

--- src/Class5/HomeWork5.java
+++ src/Class5/HomeWork5.java
@@ -1,7 +1,15 @@
 package Class5;
 
-public class HomeWork5 {
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
+public class HomeWork5 {
+  public static void main(String[] args) {
+    new Q5_1().exec();
+    new Q5_2().exec();
+    new Q5_3().exec();
+  }
 }
 
 
@@ -25,18 +33,127 @@ public class HomeWork5 {
  * </pre>
  */
 class Q5_1 {
+  public void exec(){
+    this.doStringPartOne();
+    this.doStringBufferPartOne();
+    this.doStringBuilderPartOne();
+    this.doStringPartTwo();
+    this.doStringBufferPartTwo();
+    this.doStringBuilderPartTwo();
+  }
+  
+  private void doStringPartOne(){
+    System.out.println("doStringPartOne");
+    String originStr = "I have a pen.";
+    System.out.println("originStr = " + originStr);
+    String replaceStr = originStr.replaceAll("pen", "apple");
+    System.out.println("replaceStr = " + replaceStr);
+    System.out.println("-------------");
+  }
+  
+  
+  private void doStringBufferPartOne(){
+    System.out.println("doStringBufferPartOne");
+    StringBuffer originStr = new StringBuffer("I have a pen.");
+    System.out.println("originStr = " + originStr);
+    StringBuffer replaceStr = originStr.replace(originStr.indexOf("pen"), originStr.length()-1, "apple");
+    System.out.println("replaceStr = " + replaceStr);
+    System.out.println("-------------");
+  }
+  
+  
+  private void doStringBuilderPartOne(){
+    System.out.println("doStringBuilderPartOne");
+    StringBuilder originStr = new StringBuilder("I have a pen.");
+    System.out.println("originStr = " + originStr);
+    StringBuilder replaceStr = originStr.replace(originStr.indexOf("pen"), originStr.length()-1, "apple");
+    System.out.println("replaceStr = " + replaceStr);
+    System.out.println("-------------");
+  }
+  
+  
+  private void doStringPartTwo(){
+    System.out.println("doStringPartTwo");
+    String originStr = "I have a pen.";
+    System.out.println("originStr = " + originStr);
+    String replaceStr = new String(originStr).replaceAll("pen", "apple");
+    System.out.println("replaceStr = " + replaceStr);
+    String concatStr = originStr.replaceAll("\\.", " , ") + replaceStr;
+    System.out.println("concatStr = " + concatStr);
+    System.out.println("-------------");
+  }
+  
+  
+  private void doStringBufferPartTwo(){
+    System.out.println("doStringBufferPartTwo");
+    StringBuffer originStr = new StringBuffer("I have a pen.");
+    System.out.println("originStr = " + originStr);
+    StringBuffer replaceStr = new StringBuffer(originStr).replace(originStr.indexOf("pen"), originStr.length()-1, "apple");
+    System.out.println("replaceStr = " + replaceStr);
+    StringBuffer concatStr = originStr.replace(originStr.indexOf("."), originStr.length(), " , ").append(replaceStr);
+    System.out.println("concat = " + concatStr);
+    System.out.println("-------------");
+  }
+  
+  
+  private void doStringBuilderPartTwo(){
+    System.out.println("doStringBuilderPartTwo");
+    StringBuilder originStr = new StringBuilder("I have a pen.");
+    System.out.println("originStr = " + originStr);
+    StringBuilder replaceStr = new StringBuilder(originStr).replace(originStr.indexOf("pen"), originStr.length()-1, "apple");
+    System.out.println("replaceStr = " + replaceStr);
+    StringBuilder concatStr = originStr.replace(originStr.indexOf("."), originStr.length(), " , ").append(replaceStr);
+    System.out.println("concat = " + concatStr);
+    System.out.println("-------------");
+  }
   
 }
 
 /**
  * <pre>
  *  使用Date物件與Calendar物件
- *  計算下一個年度的4/1
+ *  計算下一個年度2018的4/1
  *  那天是禮拜幾
+ *  以及離現在還有多少天
  * </pre>
  */
 class Q5_2 {
   
+  public void exec(){
+    Calendar calendar = this.setTime(2018,3,1);
+    System.out.println(this.format(calendar));
+    this.dayBtween(calendar);
+  }
+  
+  private Calendar setTime(int year , int month , int day){
+    Calendar c = Calendar.getInstance();
+    c.set(year, month, day);
+    return c;
+  }
+  
+  // calendar可以轉型成date
+  // 並使用SimpleDateFormat
+  // 進行時間的格式化
+  private String format(Calendar calendar){
+    SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd EEEE HH:mm:ss.SSS");
+    Date date = calendar.getTime();
+    return df.format(date);
+  }
+  
+  public void dayBtween(Calendar calendar){
+    Calendar now = Calendar.getInstance();
+    System.out.println("相差 " + daysBetweenCount(now , calendar) + " 天");
+  }
+  
+  private long daysBetweenCount(Calendar begin, Calendar end) {
+    Calendar calendar = (Calendar) begin.clone(); // 複製
+    long daysBetween = 0;
+    while(calendar.before(end)) {
+      calendar.add(Calendar.DAY_OF_MONTH, 1);
+      daysBetween++;
+    }
+    return daysBetween;
+  }
 }
 
 /**
@@ -53,7 +170,41 @@ class Q5_2 {
  * </pre>
  */
 class Q5_3 {
+  public void exec(){
+    printlnCalendar(2017);
+  }
   
+  private void printlnCalendar(int year){
+    System.out.println("--------------------------");
+    Calendar now = Calendar.getInstance();
+    now.set(Calendar.YEAR , year);
+    now.set(Calendar.MONTH , 0);
+    now.set(Calendar.DATE , 1);
+    for(int index = 0 ; index < 12 ; index ++){
+      System.out.println(now.get(Calendar.YEAR) + "年" + (now.get(Calendar.MONTH) + 1 ) + "月");
+      System.out.println("日\tㄧ\t二\t三\t四\t五\t六");
+      int theMonth = now.get(Calendar.MONTH) ;
+      
+      int spaceCount = now.get(Calendar.DAY_OF_WEEK);
+      for(int tag = 0 ; tag < spaceCount - 1 ; tag++){
+        System.out.print("\t");
+      }
+      
+      while(true){
+        System.out.print((now.get(Calendar.DATE) <10  ? " " + now.get(Calendar.DATE) :  now.get(Calendar.DATE) )+ "\t");
+        now.add(Calendar.DATE, 1);
+        if(theMonth!= now.get(Calendar.MONTH)){
+          System.out.println();
+          break;
+        }
+        if(now.get(Calendar.DAY_OF_WEEK) == 1){
+          System.out.print("\n");
+        }
+        System.out.print("");
+      }
+      System.out.print("----------------------\n");
+    }
+  }
 }
 
 /**
