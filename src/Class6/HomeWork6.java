296d6964a69ddea682ae4ebb75c90e5ed4e08369
STAR-PC\STAR <qkpigstar@gmail.com>
2017-04-01 22:22:40 +0800

    作業六完成

--- src/Class6/HomeWork6.java
+++ src/Class6/HomeWork6.java
@@ -3,6 +3,7 @@ package Class6;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
@@ -13,6 +14,8 @@ public class HomeWork6 {
     new Q6_1().exec();
     new Q6_2().exec();
     new Q6_3().exec();
+    new Q6_4().exec();
+    new Q6_5().exec();
   }
 }
 
@@ -227,8 +230,8 @@ class GenRandomBigLotter{
  * 1.用會員編號查詢會員買的商品
  *   輸出格式 => 編號 姓名 訂單 訂購內容 金額
  *   
- * 2.算出每位會員的平均消費金額
- *   輸出格式 => 編號 姓名 平均金額
+ * 2.算出每位會員的平均每筆消費金額
+ *   輸出格式 => 編號 姓名 平均消費金額
  *   
  * 3.依照消費總金額高到低排序
  *   輸出格式 => 編號 姓名 消費總金額
@@ -244,25 +247,121 @@ class Q6_4{
   public void exec(){
     members = new GenMemberData().initMemberMapData();
     infos = new GenOrderInfoData().initOrderInfoMapData();
+    this.printMemberBuy();
+    this.printAvgCost();
+    this.printHighCostOrder();
+    this.printLowCostOrder();
   }
   private void printMemberBuy(){
+    System.out.println("printMemberBuy");
     for(String no : members.keySet()){
       for(String key : infos.keySet()){
         if(no.equals(infos.get(key).getNo())){
-          System.out.println("編號:" + no + ", 姓名:"+);
+          System.out.println("編號:" + no + 
+                             " , 姓名:" + members.get(no).getName() + 
+                             " , 訂單號碼:" + infos.get(key).getNo() +
+                             " , 訂購內容:" + infos.get(key).getOrderDesc() +
+                             " , 金額:" + infos.get(key).getAmount());
         }
       }
     }
+    System.out.println("---------------");
   }
   private void printAvgCost(){
-    
+    System.out.println("printAvgCost");
+    for(String no : members.keySet()){
+      System.out.print("編號:" + no + 
+                       " , 姓名:" + members.get(no).getName() );
+      float totalAmout = 0 ; 
+      float count = 0 ;
+      for(String key : infos.keySet()){
+        if(no.equals(infos.get(key).getNo())){
+          count++;
+          totalAmout = totalAmout + infos.get(key).getAmount();
+        }
+      }
+      float avgAmount = totalAmout / count; 
+      System.out.printf(" , 平均消費金額 : %.2f \n" , avgAmount);
+    }
+    System.out.println("---------------");
   }
+  
   private void printHighCostOrder(){
-    
+    System.out.println("printHighCostOrder");
+    // 利用總金額當作Key，value儲存會員編號
+    Map<Integer , String> sumMemberPayMap = sortSumMemberPayMap();
+    // 用陣列方式，放入總金額
+    Integer[] orderByHighAmount = addMemberAmount(sumMemberPayMap);
+    // 根據總金額開始排序
+    for(int index = 0 ; index < orderByHighAmount.length-1 ; index++){
+      for(int tag = 1 ; tag < orderByHighAmount.length ;tag++){
+        if(orderByHighAmount[index] < orderByHighAmount[tag]){
+          swap(orderByHighAmount , index , tag);
+        }
+      }
+    }
+    // 利用總金額的排序，利用sumMemberPayMap，去找出會員編號，印出相關資料
+    this.printOrderAmount(orderByHighAmount, sumMemberPayMap);
+    System.out.println("---------------");
   }
+  
+  private LinkedHashMap <Integer , String> sortSumMemberPayMap(){
+    LinkedHashMap<Integer , String> sumMemberPayMap = new LinkedHashMap<Integer , String>();
+    for(String no : members.keySet()){
+      int totalAmt = 0 ;
+      for(String key : infos.keySet()){
+        if(no.equals(infos.get(key).getNo())){
+          totalAmt = totalAmt + infos.get(key).getAmount();
+        }
+      }
+      sumMemberPayMap.put(totalAmt,no);
+    }
+    return sumMemberPayMap;
+  }
+  
+  private Integer[] addMemberAmount(Map<Integer , String> sumMemberPayMap){
+    Integer[] orderByHighAmount = new Integer[sumMemberPayMap.size()];
+    int count = 0;
+    for(Integer totalAmt :sumMemberPayMap.keySet()){
+      orderByHighAmount[count] = totalAmt; 
+      count++;
+    }
+    return orderByHighAmount;
+  }
+  
+  private void printOrderAmount(Integer[] orderByHighAmount , Map<Integer , String> sumMemberPayMap){
+    // 利用總金額的排序，利用sumMemberPayMap，去找出會員編號，印出相關資料
+    for(int index = 0 ; index < orderByHighAmount.length ; index++){
+      String no = sumMemberPayMap.get(orderByHighAmount[index]);
+      System.out.println("編號 : " + no + " , 姓名 : " + members.get(no).getName() + " , 消費總金額  : " + orderByHighAmount[index]);
+    }
+  }
+  
+  private void swap(Integer[] totalAmt , int index , int changeIndex){
+    Integer temp = totalAmt[index];
+    totalAmt[index] = totalAmt[changeIndex];
+    totalAmt[changeIndex] = temp;
+  }
+  
   private void printLowCostOrder(){
-    
+    System.out.println("printLowCostOrder");
+    // 利用總金額當作Key，value儲存會員編號
+    Map<Integer , String> sumMemberPayMap = sortSumMemberPayMap();
+    // 用陣列方式，放入總金額
+    Integer[] orderByHighAmount = addMemberAmount(sumMemberPayMap);
+    // 根據總金額開始排序
+    for(int index = 0 ; index < orderByHighAmount.length-1 ; index++){
+      for(int tag = 1 ; tag < orderByHighAmount.length ;tag++){
+        if(orderByHighAmount[index] > orderByHighAmount[tag]){
+          swap(orderByHighAmount , index , tag);
+        }
+      }
+    }
+    // 利用總金額的排序，利用sumMemberPayMap，去找出會員編號，印出相關資料
+    this.printOrderAmount(orderByHighAmount, sumMemberPayMap);
+    System.out.println("---------------");
   }
+  
 }
 
 /**
@@ -280,7 +379,7 @@ class Q6_4{
  * 訂單資料
  * 訂單 會員編號  訂購內容     金額
  * O001     C1      衣服        789
- * O002     C1        3C       1999
+ * O002     C1        3C    1999
  * O003     C2      遊戲       1899
  * O004     C2    保養品       3300
  * O005     C3    攝影機      14999
@@ -288,11 +387,11 @@ class Q6_4{
  * 請用新增會員與訂單類別(Class)
  * 設定每位會員資料以及訂單資料後
  * 
- * 1.用會員編號找出會員買的商品
+ * 1.用會員編號查詢會員買的商品
  *   輸出格式 => 編號 姓名 訂單 訂購內容 金額
  *   
- * 2.算出每位會員的平均消費金額
- *   輸出格式 => 編號 姓名 平均金額
+ * 2.算出每位會員的平均每筆消費金額
+ *   輸出格式 => 編號 姓名 平均消費金額
  *   
  * 3.依照消費總金額高到低排序
  *   輸出格式 => 編號 姓名 消費總金額
@@ -309,18 +408,117 @@ class Q6_5{
   public void exec(){
     members = new GenMemberData().initMemberListData();
     infos = new GenOrderInfoData().initOrderInfoListData();
+    this.printMemberBuy();
+    this.printAvgCost();
+    this.printHighCostOrder();
+    this.printLowCostOrder();
   }
+  
   private void printMemberBuy(){
-    
+    System.out.println("printMemberBuy");
+    for(int index = 0 ; index < members.size() ; index++){
+      for(int tag = 0 ; tag < infos.size() ; tag++){
+        if(members.get(index).getNo().equals(infos.get(tag).getNo())){
+          System.out.println("編號:" + members.get(index).getNo() + 
+                             " , 姓名:" + members.get(index).getName() + 
+                             " , 訂單號碼:" + infos.get(tag).getNo() +
+                             " , 訂購內容:" + infos.get(tag).getOrderDesc() +
+                             " , 金額:" + infos.get(tag).getAmount());
+        }
+      }
+    }
+    System.out.println("---------------");
   }
+  
   private void printAvgCost(){
-    
+    System.out.println("printAvgCost");
+    for(Member mem : members){
+      System.out.print("編號:" + mem.getNo() + 
+                       " , 姓名:" + mem.getName() );
+      float totalAmout = 0 ; 
+      float count = 0 ;
+      for(OrderInfo orderInfo : infos){
+        if(mem.getNo().equals(orderInfo.getNo())){
+          count++;
+          totalAmout = totalAmout + orderInfo.getAmount();
+        }
+      }
+      float avgAmount = totalAmout / count; 
+      System.out.printf(" , 平均消費金額 : %.2f \n" , avgAmount);
+    }
+    System.out.println("---------------");
   }
+  
   private void printHighCostOrder(){
-    
+    System.out.println("printHighCostOrder");
+    // 利用逗點，串聯資料
+    // ex : 會員編號,總金額
+    String[] concatData = this.concatData();
+    // 進行排序
+    for(int index = 0 ; index < concatData.length-1 ; index++){
+      for(int tag = 1 ; tag < concatData.length ; tag++){
+        if(Integer.parseInt(concatData[index].split(",")[1]) 
+           <
+           Integer.parseInt(concatData[tag].split(",")[1]) ){
+          swap(concatData , index , tag);
+        }
+      }
+    }
+    this.printOrderAmount(concatData);
+    System.out.println("---------------");
   }
+  
+  private String[] concatData(){
+    String[] concatData = new String[members.size()];
+    int count = 0 ;
+    for(Member mem : members){
+      int totalAmout = 0 ; 
+      for(OrderInfo orderInfo : infos){
+        if(mem.getNo().equals(orderInfo.getNo())){
+          totalAmout = totalAmout + orderInfo.getAmount();
+        }
+      }
+      concatData[count] = mem.getNo()+","+totalAmout;
+      count++;
+    }
+    return concatData;
+  }
+  
+  private void printOrderAmount(String[] concatData){
+    for(int index = 0 ; index < concatData.length ; index++){
+      String no = concatData[index].split(",")[0];
+      Integer totalAmount = Integer.parseInt(concatData[index].split(",")[1]);
+      for(Member mem : members){
+        if(no.equals(mem.getNo())){
+          System.out.println("編號 : " + no + " , 姓名 : " + mem.getName() + " , 消費總金額  : " + totalAmount);
+        }
+      }
+    }
+  }
+  
+  private void swap (String[] concatData , int index , int changeIndex){
+    String temp = concatData[index];
+    concatData[index] = concatData[changeIndex];
+    concatData[changeIndex] = temp;
+  }
+  
   private void printLowCostOrder(){
-    
+    System.out.println("printLowCostOrder");
+    // 利用逗點，串聯資料
+    // ex : 會員編號,總金額
+    String[] concatData = this.concatData();
+    // 進行排序
+    for(int index = 0 ; index < concatData.length-1 ; index++){
+      for(int tag = 1 ; tag < concatData.length ; tag++){
+        if(Integer.parseInt(concatData[index].split(",")[1]) 
+           >
+           Integer.parseInt(concatData[tag].split(",")[1]) ){
+          swap(concatData , index , tag);
+        }
+      }
+    }
+    this.printOrderAmount(concatData);
+    System.out.println("---------------");
   }
 }
 
