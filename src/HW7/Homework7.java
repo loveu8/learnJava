d9809916ba7031e46354c9f6bef9e0648b8d4b53
LornGhu <lornghu@eztravel.com.tw>
2017-05-15 16:46:23 +0800

    實作中

--- src/HW7/Homework7.java
+++ src/HW7/Homework7.java
@@ -1,10 +1,12 @@
 package HW7;
 
-import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-
-
+import java.util.Scanner;
+import java.util.Set;
 
 public class Homework7 {
   public static void main(String[] args) {
@@ -65,13 +67,147 @@ class Q7_1 {
 
   // 建立一個drinkInfo
   Map<String, Drink> drinkInfo; 
+  
+  // 建立訂單資料
+  LinkedList<Order> orders;
 
   // 執行
   public void exec() {
+    genDrinkInfo();
+    genDefaultOrders();
+    genOrderSystem();
+  }
+  
+
+  private void genDrinkInfo(){
     drinkInfo = new GenDrinkInfo().initdrinkData(); 
     for (String key : drinkInfo.keySet()) {
       System.out.println(drinkInfo.get(key).toString());
     }
+  }
+  
+  private void genDefaultOrders() {
+    orders = new LinkedList<Order>();
+  }
+  
+  private void genOrderSystem(){
+    boolean system = true;
+    
+    while(system){
+      System.out.println("飲料點餐系統");
+      Scanner scanner = new Scanner(System.in);
+      System.out.println("1.點餐系統，請按1");
+      System.out.println("2.訂單查詢，請按2");
+      System.out.println("0.結束功能，請按0");
+      System.out.println("請輸入要執行的功能:");
+      if(!scanner.hasNextInt()){
+        System.out.println("輸入錯誤，重新開始\n");
+        continue;
+      }
+      int functionFlag = Integer.parseInt(scanner.next()); 
+      switch(functionFlag){
+        case 1:
+          orderDrink();
+          break;
+        case 2:
+          
+          break;
+        case 0:
+          system = false;
+          System.out.println("飲料點餐系統結束");
+          break;
+        default:
+          System.out.println("您尚未選取到任何功能，請重新開始");
+          break;
+      }
+    }
+  }
+  
+  private void orderDrink(){
+    boolean orderDrinkSystem = true;
+    Order order = new Order();
+    while(orderDrinkSystem){
+      Scanner scanner = new Scanner(System.in);
+      // 姓名
+      System.out.print("請輸入顧客姓名(離開請按0):");
+      String input = scanner.next();
+      if("0".equals(input)){
+        System.out.println("取消點餐");
+        orderDrinkSystem = false;
+        break;
+      }
+      order.setCusName(input);
+      
+      // 電話
+      System.out.print("請輸入顧客電話(離開請按0):");
+      input = scanner.next();
+      if("0".equals(input)){
+        System.out.println("取消點餐");
+        orderDrinkSystem = false;
+        break;
+      }
+      order.setCusTelphone(input);
+      
+      // 外帶還內用
+      System.out.print("外帶請按1，內用請按2(離開請按0):");
+      input = scanner.next();
+      if("0".equals(input)){
+        System.out.println("取消點餐");
+        orderDrinkSystem = false;
+        break;
+      }
+      if("1".equals(input)){
+        order.setToGo(true);
+      } else {
+        order.setToGo(false);
+      }
+      
+      // 地址
+      if(order.isToGo()){
+        System.out.println("請輸入顧客地址(離開請按0):");
+        input = scanner.next();
+        if("0".equals(input)){
+          System.out.println("取消點餐");
+          orderDrinkSystem = false;
+          break;
+        }
+        order.setAddress(input);
+      }
+      
+      // 顯示飲料清單
+      boolean drinkOrderSystem = true;
+      while(drinkOrderSystem){
+        Map<String, Drink> drinkInfoDetail = new GenDrinkInfo().initdrinkData();
+        Set<String> prodNumbers = new HashSet<String>();
+        for(String key : drinkInfoDetail.keySet()){
+          System.out.println("編號 : " + drinkInfoDetail.get(key).getProdNumber()+ 
+                             " 飲料 : " + drinkInfoDetail.get(key).getName() + 
+                             " 大小 : " + drinkInfoDetail.get(key).getBigsmall() + 
+                             " 價格 : " + drinkInfoDetail.get(key).getPrice());
+          prodNumbers.add(drinkInfoDetail.get(key).getProdNumber());
+        }
+        System.out.println("請選擇飲料編號(離開請按0):");
+        input = scanner.next();
+        boolean isTheProdNumber = false; 
+        for(String prodNumber :prodNumbers){
+          if(input.equals(prodNumber)){
+            isTheProdNumber = true;
+            break;
+          }
+        }
+        if(!isTheProdNumber){
+          System.out.println("請選擇正確的飲料");
+          continue;
+        }
+        
+        drinkOrderSystem = false;
+      }
+      orderDrinkSystem = false;
+    }
+  }
+  
+  private void orderQuery(){
+    
   }
   
 }
@@ -112,8 +248,9 @@ public void setting(String sugar , boolean hot , String ice , int number);
 class Drink extends Food implements SetDrinkAction {
   
   // 初始不可變動飲料參數
-  public Drink(String name , String desc , String raw , String bigsmall , int price , String ml , String kcal) {
+  public Drink(String prodNumber , String name , String desc , String raw , String bigsmall , int price , String ml , String kcal) {
     super(name, desc);
+    setProdNumber(prodNumber);
     setRaw(raw);
     setBigsmall(bigsmall);
     setPrice(price);
@@ -121,6 +258,7 @@ class Drink extends Food implements SetDrinkAction {
     setKcal(kcal);
   }
   // 不可變動
+  private String prodNumber;
   private String raw;
   private String bigsmall;
   private int price;
@@ -132,6 +270,14 @@ class Drink extends Food implements SetDrinkAction {
   private String ice;
   private String sugar;
   private int number;
+
+  public String getProdNumber() {
+    return prodNumber;
+  }
+
+  public void setProdNumber(String prodNumber) {
+    this.prodNumber = prodNumber;
+  }
 
   public String getRaw() {
     return raw;
@@ -250,11 +396,11 @@ enum DrinkName {
 
 class GenDrinkInfo {
   public Map<String, Drink> initdrinkData() { // initdrinkData方法名稱
-    Map<String, Drink> drink = new HashMap<String, Drink>();
-    drink.put("大正紅茶-L", new Drink(DrinkName.大正紅茶.getName() , "茶葉  原產地：印度", "茶葉" , "L" , 30 , "700", "140"));
-    drink.put("初露青茶-L", new Drink(DrinkName.初露青茶.getName() , "茶葉  原產地：台灣", "茶葉" , "L" , 30 , "700", "140"));
-    drink.put("珍珠紅茶拿鐵-M", new Drink(DrinkName.珍珠紅茶拿鐵.getName() , "茶葉  原產地：印度", "茶葉 , 牛奶 , 珍珠" , "M" , 50 , "500", "100"));
-    drink.put("珍珠紅茶拿鐵-L", new Drink(DrinkName.珍珠紅茶拿鐵.getName() , "茶葉  原產地：印度", "茶葉 , 牛奶 , 珍珠" , "L" , 60 , "700", "120"));
+    Map<String, Drink> drink = new LinkedHashMap<String, Drink>();
+    drink.put("大正紅茶-L", new Drink("1" , DrinkName.大正紅茶.getName() , "茶葉  原產地：印度", "茶葉" , "L" , 30 , "700", "140"));
+    drink.put("初露青茶-L", new Drink("2" ,DrinkName.初露青茶.getName() , "茶葉  原產地：台灣", "茶葉" , "L" , 30 , "700", "140"));
+    drink.put("珍珠紅茶拿鐵-M", new Drink("3" ,DrinkName.珍珠紅茶拿鐵.getName() , "茶葉  原產地：印度", "茶葉 , 牛奶 , 珍珠" , "M" , 50 , "500", "100"));
+    drink.put("珍珠紅茶拿鐵-L", new Drink("4" ,DrinkName.珍珠紅茶拿鐵.getName() , "茶葉  原產地：印度", "茶葉 , 牛奶 , 珍珠" , "L" , 60 , "700", "120"));
     return drink;
   }
 }
